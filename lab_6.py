d4a35e0b48cd1d9d9ee4d225aee0f47bb8708e03
Kevin <k.zheng@ufl.edu>
2023-03-06 20:38:27 -0500

    This is my encode function along with my main program.

--- lab_6.py
+++ lab_6.py
@@ -1,3 +1,4 @@
+# Kevin Zheng
 def encode(string):
     char_list = []
     encode_string = ""
@@ -14,8 +15,32 @@ def encode(string):
     return encode_string
 
 
+def print_options():
+    print("\n0. Exit")
+    print("1. Encode")
+    print("2. Decode\n")
+
+
 def main():
-    print("Hello World")
+    print_options()
+
+    user_input = int(input("Select from the following options: "))
+    num = 0
+
+    while user_input != 0:
+        if user_input == 1:
+            num = input("Enter a number to be encoded: ")
+            encode_num = encode(num)
+            print(encode_num)
+
+        if user_input == 2:
+            num = input("Enter a number to be decoded: ")
+            # decode_num = decode(num)
+            # print(decode_num)
+
+        print_options()
+
+        user_input = int(input("Select from the following options: "))
 
 
 if __name__ == "__main__":
